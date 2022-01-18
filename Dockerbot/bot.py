f79c3e82cb07cc7580cc7990d0dcf2f4b5127864
Pixel-Therapy <41266240+Pixel-Therapy@users.noreply.github.com>
2022-01-18 04:01:10 +0100

    working basic discord bot

--- Dockerbot/bot.py
+++ Dockerbot/bot.py
@@ -3,10 +3,16 @@ import asyncio
 import http
 import websockets
 from tokens import TOKEN
+from util import id_generator
+from collections import defaultdict
+
+# DISCORD PART
+
+routing_table = defaultdict(lambda: None)  # Routes guild_ids to websockets
+pending_sockets = defaultdict(lambda: None)  #
 
 
-
-class MyClient(discord.Client):
+class DiscordClient(discord.Bot):
     # This is here for debugging purposes
     async def on_ready(self):
         print("Logged on as {0}!".format(self.user))
@@ -17,40 +23,79 @@ class MyClient(discord.Client):
 
     # Triggers whenever a user enters or leaves a voice channel
     async def on_voice_state_update(self, user, before, after):
-        if after.channel is None:
-            print(user.name + " left channel ", before.channel) 
-        else:           
-            print(user.name + " has joined " + after.channel.name)
+        # If the user has joined a voice channel
+        if before.channel is None and after.channel is not None:
+            state = "joined"
+            channel = after.channel
+            
+        # If the user has left a voice channel
+        elif before.channel is not None and after.channel is None:
+            state = "left"
+            channel = before.channel
+
         
-        
+        message = f"{user} has {state} {channel.name}"
+        print(message)
+
+        # Check if there are receiving sockets for this guild
+        if (sockets := routing_table[channel.guild.id]) is not None:
+            # If there are, send the websockets a message
+            coros = [sock.send(message) for sock in sockets]
+            await asyncio.gather(*coros)
 
 
-client = MyClient()
+bot = DiscordClient(sync_commands=False)
 
 
+@bot.slash_command()
+async def connect_ipane(ctx, key: str):
+    if websocket := pending_sockets[key]:
+        if (
+            routing_table[ctx.guild.id] is None
+        ):  # If there are no sockets for this guild
+            routing_table[ctx.guild.id] = [
+                websocket
+            ]  # Add the socket to the routing table in a list
+        else:
+            routing_table[ctx.guild.id].append(websocket)
+
+        await websocket.send("connection_accepted")
+        await ctx.respond("Connection accepted")
+        pending_sockets.pop(key)
+        print(f"{ctx.author}'s Ipane is connected to {ctx.guild.name}")
+    else:
+        await ctx.respond("key not found")
 
 
+# WEBSOCKET PART
 async def health_check(path, request_headers):
     if path == "/healthz":
         return http.HTTPStatus.OK, [], b"OK\n"
 
 
-async def echo(websocket):
+async def receive_ws(websocket):
+
     async for message in websocket:
-        await websocket.send(message)
+        if message.startswith("connect"):
+            key = message.split(":")[1]
+            pending_sockets[key] = websocket
+        await websocket.send("connection_pending")
 
 
-async def run_websock():
+# TODO implement a health check for the connected ipanes
+
+
+async def ws_server_run():
     async with websockets.serve(
-        echo,
+        receive_ws,
         "localhost",
         8765,
         process_request=health_check,
     ):
-        await asyncio.Future() # run forever
+        await asyncio.Future()  # run forever
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    asyncio.gather(run_websock(), client.start(TOKEN))
+    asyncio.gather(ws_server_run(), bot.start(TOKEN))
     loop.run_forever()
-
